1dc701b9aaa510ac87c4758d220f8f87d2b9ecbb
Smit Solanki <ssolanki@horizontal.com>
2024-09-03 18:54:50 +0530

    userlocation-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css"; // Import your CSS file
 import FindABranchTitle from "./components/Title";
 import CustomMapComponent from "./components/CustomMapComponent";
@@ -9,6 +9,25 @@ import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
   const libraries = ["places"];
+
+  useEffect(() => {
+    const saveUserLocation = () => {
+      if (!localStorage.getItem("userLocation")) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            const userLocation = { lat: latitude, lng: longitude };
+            localStorage.setItem("userLocation", JSON.stringify(userLocation));
+          },
+          (error) => {
+            console.error("Error getting user location:", error);
+          }
+        );
+      }
+    };
+
+    saveUserLocation();
+  }, []);
   return (
     <div className="App">
       <FindABranchTitle />
