ac1aa6a8b09da0a728dd00f28bde76e53d571696
Smit Solanki <ssolanki@horizontal.com>
2024-09-06 13:13:00 +0530

    added-markeroverlay

--- src/components/CustomMapComponent.jsx
+++ src/components/CustomMapComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { GoogleMap, Marker } from "@react-google-maps/api";
+import { GoogleMap, Marker, OverlayView } from "@react-google-maps/api";
 import EventBus from "../EventBus";
 import { useData } from "../DataContext";
 
@@ -12,6 +12,7 @@ function CustomMapComponent() {
   const [markers, setMarkers] = useState([]);
   const [center, setCenter] = useState({ lat: 0, lng: 0 });
   const [selectedMarkerId, setSelectedMarkerId] = useState(null);
+  const [selectedMarkerDetails, setSelectedMarkerDetails] = useState(null);
   const [googleMaps, setGoogleMaps] = useState(null);
   const markersRef = useRef([]);
   const mapRef = useRef(null);
@@ -24,6 +25,11 @@ function CustomMapComponent() {
       location: {
         lat: parseFloat(item.location[0]),
         lng: parseFloat(item.location[1]),
+      },
+      details: {
+        title: item.title,
+        address: item.address,
+        categories: item.categories,
       },
     }));
     setMarkers(newMarkers);
@@ -45,6 +51,7 @@ function CustomMapComponent() {
       google.maps.event.addListenerOnce(mapRef.current, "idle", () => {
         mapRef.current.setZoom(10); // Adjust zoom level as needed
       });
+      setSelectedMarkerDetails(marker.details); // Set the details of the selected marker
     }
   };
 
@@ -61,6 +68,7 @@ function CustomMapComponent() {
     EventBus.on("listItemClicked", handleListItemClick);
     EventBus.on("resetMap", () => {
       setSelectedMarkerId(null);
+      setSelectedMarkerDetails(null); // Clear selected marker details
       EventBus.emit("markerClicked", null);
     });
     return () => {
@@ -68,6 +76,7 @@ function CustomMapComponent() {
       EventBus.off("listItemClicked", handleListItemClick);
       EventBus.off("resetMap", () => {
         setSelectedMarkerId(null);
+        setSelectedMarkerDetails(null); // Clear selected marker details
       });
     };
   }, []);
@@ -110,12 +119,63 @@ function CustomMapComponent() {
       }}
     >
       {markers.map((position, index) => (
-        <Marker
-          key={index}
-          icon={position?.id === selectedMarkerId ? selectedIcon : defaultIcon}
-          position={position.location}
-          onClick={() => handleMarkerClick(position.id)}
-        />
+        <>
+          <Marker
+            key={index}
+            icon={
+              position?.id === selectedMarkerId ? selectedIcon : defaultIcon
+            }
+            position={position.location}
+            onClick={() => handleMarkerClick(position.id)}
+          >
+            {position.id === selectedMarkerId && selectedMarkerDetails && (
+              <OverlayView
+                position={position.location}
+                mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+                getPixelPositionOffset={(width, height) => ({
+                  x: -(width / 2),
+                  y: -height,
+                })}
+              >
+                <div
+                  style={{
+                    background: "white",
+                    border: "1px solid #ccc",
+                    padding: "10px",
+                    borderRadius: "8px",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                    maxWidth: "200px",
+                    width: "fit-content",
+                    fontFamily: "'Roboto', sans-serif",
+                  }}
+                >
+                  <h4
+                    style={{
+                      margin: "0 0 5px",
+                      fontSize: "16px",
+                      color: "#333",
+                    }}
+                  >
+                    {selectedMarkerDetails.title}
+                  </h4>
+                  <p
+                    style={{
+                      margin: "0 0 5px",
+                      fontSize: "14px",
+                      color: "#666",
+                      textWrap: "nowrap",
+                    }}
+                  >
+                    {selectedMarkerDetails.address}
+                  </p>
+                  <p style={{ margin: "0", fontSize: "14px", color: "#666" }}>
+                    {selectedMarkerDetails.categories.join(", ")}
+                  </p>
+                </div>
+              </OverlayView>
+            )}
+          </Marker>
+        </>
       ))}
     </GoogleMap>
   );
